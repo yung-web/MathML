66e5c7974a13caa96650448ac539ca2fed187d27
yung-web <81741647+yung-web@users.noreply.github.com>
2021-04-02 10:44:40 +0900

    ...

---
+++ print.sh
@@ -0,0 +1,44 @@
+pdfjam --nup 1x2 01.intro.pdf --outfile 01.intro-2.pdf
+pdfjam --nup 2x2 01.intro.pdf --outfile 01.intro-4.pdf --landscape
+
+pdfjam --nup 1x2 02.nf-pure.pdf --outfile 02.nf-pure-2.pdf
+pdfjam --nup 2x2 02.nf-pure.pdf --outfile 02.nf-pure-4.pdf --landscape
+
+pdfjam --nup 1x2 03.nf-mixed.pdf --outfile 03.nf-mixed-2.pdf
+pdfjam --nup 2x2 03.nf-mixed.pdf --outfile 03.nf-mixed-4.pdf --landscape
+
+pdfjam --nup 1x2 04.nf-eff-cont.pdf --outfile 04.nf-eff-cont-2.pdf
+pdfjam --nup 2x2 04.nf-eff-cont.pdf --outfile 04.nf-eff-cont-4.pdf --landscape
+
+pdfjam --nup 1x2 05.Potential.pdf --outfile 05.Potential-2.pdf
+pdfjam --nup 2x2 05.Potential.pdf --outfile 05.Potential-4.pdf --landscape
+
+pdfjam --nup 1x2 05.Potential.pdf --outfile 05.Potential-2.pdf
+pdfjam --nup 2x2 05.Potential.pdf --outfile 05.Potential-4.pdf --landscape
+
+pdfjam --nup 1x2 06.playingwitheq.pdf --outfile 06.playingwitheq-2.pdf
+pdfjam --nup 2x2 06.playingwitheq.pdf --outfile 06.playingwitheq-4.pdf --landscape
+
+pdfjam --nup 1x2 07.ef.pdf --outfile 07.ef-2.pdf
+pdfjam --nup 2x2 07.ef.pdf --outfile 07.ef-4.pdf --landscape
+
+pdfjam --nup 1x2 08.Stackelberg.pdf --outfile 08.Stackelberg-2.pdf
+pdfjam --nup 2x2 08.Stackelberg.pdf --outfile 08.Stackelberg-4.pdf --landscape
+
+pdfjam --nup 1x2 09.repeated.pdf --outfile 09.repeated-2.pdf
+pdfjam --nup 2x2 09.repeated.pdf --outfile 09.repeated-4.pdf --landscape
+
+pdfjam --nup 1x2 10.Bayesian.pdf --outfile 10.Bayesian-2.pdf
+pdfjam --nup 2x2 10.Bayesian.pdf --outfile 10.Bayesian-4.pdf --landscape
+
+pdfjam --nup 1x2 11.PE.pdf --outfile 11.PE-2.pdf
+pdfjam --nup 2x2 11.PE.pdf --outfile 11.PE-4.pdf --landscape
+
+pdfjam --nup 1x2 12.NBS.pdf --outfile 12.NBS-2.pdf
+pdfjam --nup 2x2 12.NBS.pdf --outfile 12.NBS-4.pdf --landscape
+
+pdfjam --nup 1x2 13.SV.pdf --outfile 13.SV-2.pdf
+pdfjam --nup 2x2 13.SV.pdf --outfile 13.SV-4.pdf --landscape
+
+pdfjam --nup 1x2 14.learning.pdf --outfile 14.learning-2.pdf
+pdfjam --nup 2x2 14.learning.pdf --outfile 14.learning-4.pdf --landscape     
